5c8ff705d2611c94cba1c8b80bd7fa39bc33cf8a
RRedGamer <59791373+RRedGamer@users.noreply.github.com>
2020-01-12 17:50:47 +0800

    Update ''Talk''_here.cpp

--- ''Talk''_here.cpp
+++ ''Talk''_here.cpp
@@ -8,7 +8,53 @@ using namespace std;
 
 int main()
 {
+  int LetterCount, CurrentLetter, ScrollSpeed;
+	string ExportedWord;
 
+	fstream Dialogue;
+	Dialogue.open("Dialogue.txt", fstream::in);
 
+	if (!Dialogue)
+		cout << "Error: Dialogue file not present!" << endl;
+	else
+	{
+		while (!Dialogue.eof())
+		{
+			Dialogue >> ExportedWord;
+			LetterCount = ExportedWord.length();
 
+			if (ExportedWord.substr(0, 1) == "x")
+			{
+				if (ExportedWord.substr(1, 1) == ".")
+					ScrollSpeed = 500;
+				else
+					ScrollSpeed = 100;
+
+				CurrentLetter = 1;
+				while (LetterCount != CurrentLetter)
+				{
+					cout << ExportedWord.substr(CurrentLetter, 1);
+					Sleep(ScrollSpeed);
+					CurrentLetter++;
+				}
+			}
+			else
+			{
+				CurrentLetter = 0;
+				while (LetterCount != CurrentLetter)
+				{
+					cout << ExportedWord.substr(CurrentLetter, 1);
+					Sleep(100);
+					CurrentLetter++;
+				}
+			}
+
+			cout << " ";
+		}
+		Dialogue.close();
+	}
+
+	cout << endl;
+	system("pause");
+	return 0;
 }

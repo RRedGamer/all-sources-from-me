b5920121c680e2221b7d7b67e447d5aadf5f5577
NicholasOfTime <59541540+NicholasOfTime@users.noreply.github.com>
2020-01-21 23:40:32 +0800

    Update TecMagus'Assistant.cpp

--- TecMagus'Assistant.cpp
+++ TecMagus'Assistant.cpp
@@ -8,7 +8,8 @@
 
 using namespace std;
 
-void Continue();
+void NewGame();
+void ContinueGame();
 void LearnCpp();
 int Dialogue();
 int Chap1();
@@ -27,8 +28,7 @@ int main()
 {
 	string selection = "0";
 
-	// Game Startup
-
+	//Game Startup
 	cout << "this game is meant to be played in the current window size" << endl;
 	Sleep(5000);
 	cout << "please do not minimise or maximise the window" << endl;
@@ -36,7 +36,7 @@ int main()
 	system("cls");
 	Sleep(1000);
 
-	// Title Screen
+	//Title Screen
 	Sleep(400);
 	cout << "          __________                            ____  ___                                                        ____" << endl;
 	Sleep(400);
@@ -70,8 +70,6 @@ int main()
 	Sleep(3000);
 	system("color 8f");
 
-	// if and else statement here please @ yi xiang
-
 	cout << "     +    __________       +           *        ____  ___        *                +        +           *         ____" << endl;
 	cout << "         /__    ___/  ________   ________      /    |/   |     __________     _________   ___   ___ . ________ +/_  /" << endl;
 	cout << "       .   /   /     /  ___  /  /  _____/     /          |    /  ___    /    /  ___   /  /  /  /  /  /  _____/   /_/ " << endl;
@@ -90,7 +88,7 @@ int main()
 	Sleep(1000);
 	cout << endl;
 	cout << "                                               An educational game about C++                                            " << endl;
-	Sleep(3000); 
+	Sleep(3000);
 	cout << endl << endl;
 	cout << "                                                   1. N e w   G a m e                                                   " << endl;
 	cout << "                                                   2. C o n t i n u e                                                   " << endl;
@@ -98,7 +96,7 @@ int main()
 	cout << "                                                   4.     Q u i t                                                       " << endl;
 	Sleep(1000);
 
-	while (selection != "3")
+	while (selection != "4")
 	{
 		system("cls");
 		cout << "     +    __________       +           *        ____  ___        *                +        +           *         ____" << endl;
@@ -121,20 +119,41 @@ int main()
 		cout << "                                                   4.     Q u i t\n\n";
 		cout << "                                              Enter Number to select option: ";
 		getline(cin, selection);
-		
-		if (selection == "1")		cout << "OwO";
-		else if (selection == "2")	cout << "UwU";
-		else if (selection == "3")	cout << "EwE";
-		else if (selection == "4")	break;
+
+		if (selection == "1")
+		{
+			system("CLS");
+			NewGame();
+		}
+		else if (selection == "2")
+		{
+			system("CLS");
+			ContinueGame();
+		}
+		else if (selection == "3")
+		{
+			system("CLS");
+			LearnCpp();
+		}
+		else if (selection == "4")
+		{
+			break;
+		}
 		else
 		{
 			cout << "                                             Error computing...please try again";
-			Sleep(700);
+			Sleep(1200);
 		}
+
+
+
+
+
+
 	}
 
 	system("cls");
-	cout << "   Game closing";
+	cout << "Game closing";
 	Sleep(800);
 	cout << ".";
 	Sleep(800);
@@ -145,86 +164,293 @@ int main()
 	return 0;
 }
 
-void Continue()
+void NewGame()
 {
+	int GameState = 0, LoopForever = 0;
+	string ResetFile;
 
-	system("cls");
-	cout << endl;
-	cout << endl;
-	cout << "------------------------------------------------------------ ------------------------------------------------------------" << endl;
-	cout << "------------------------------------------------------------ ------------------------------------------------------------" << endl;
+	while (LoopForever == 0)
+	{
+		cout << "     +    __________       +           *        ____  ___        *                +        +           *         ____" << endl;
+		cout << "         /__    ___/  ________   ________      /    |/   |     __________     _________   ___   ___ . ________ +/_  /" << endl;
+		cout << "       .   /   /     /  ___  /  /  _____/     /          |    /  ___    /    /  ___   /  /  /  /  /  /  _____/   /_/" << endl;
+		cout << "    .     /   / +   /  _____/  /  /____   *  /  /|   /|  |   /  /__/    |   /  /__/  /  /  /__/  /  /_____  /  ." << endl;
+		cout << "         /___/     /_______>  /_______/     /__/ |__/ |__|  /________/|_|  /_____   /  /________/  /_______/ ." << endl;
+		cout << "       *       .       .        *       +         .       *               ______/  /            .               ." << endl;
+		cout << "   .                                +                  .         .       /________/     +                 ." << endl;
+		cout << "      ______ .           +                     +              ____     .              .     *          ____     +" << endl;
+		cout << "     /  _   |     ________ . ________   ____ + ________   ___/   /__   __________ *   __ _______ + ___/   /__ ." << endl;
+		cout << "    /  /_|  |    /  _____/  /  _____/. /   /  /  _____/  /__    ___/  /  ___    /    / // __   /  /__    ___/" << endl;
+		cout << "   /  ___   |+  /_____  /  /_____  / ./   /  /_____  /  .  /   /     /  /__/    |   /    / /  /   . /   / ." << endl;
+		cout << "  /__/   |__|  /_______/ +/_______/  /___/  /_______/     /___/   + /________/|_|  /____/ /__/     /___/     *" << endl;
+		cout << " *  .     +          .         *      .          +            .      .          +        .    .        +" << endl;
 
+		fstream SaveState;
+		SaveState.open("SaveState.txt", fstream::in);
 
+		if (!SaveState)
+		{
+			system("CLS");
+			cout << "Error accessing file" << endl;
+			break;
+		}
+		else
+			SaveState >> GameState;
+
+		SaveState.close();
+
+		if (GameState == 0)
+		{
+			cout << "\n                                                 New game starting";
+			Sleep(800);
+			cout << ".";
+			Sleep(800);
+			cout << ".";
+			Sleep(800);
+			cout << ".";
+			Sleep(800);
+
+			SaveState.open("SaveState.txt", fstream::out);
+
+			if (!SaveState)
+			{
+				system("CLS");
+				cout << "Error accessing file" << endl;
+				break;
+			}
+			else
+			{
+				SaveState << "1";
+				if (SaveState.fail())			break;
+			}
+
+			SaveState.close();
+
+			GameState = 1;
+		}
+		else if (GameState == 1 || GameState == 2)
+		{
+			if (GameState == 1)
+			{
+				cout << "\n                               A save file currently exists, do you want to override it?\n" << endl;
+			}
+			else if (GameState == 2)
+			{
+				cout << "\n                           A FULLY completed save file currently exists, do you want to override it?\n" << endl;
+			}
+			cout << "                                                         1 . Y e s" << endl;
+			cout << "                                                         2 . N o\n" << endl;
+			cout << "                                                Enter Number to select option: ";
+			getline(cin, ResetFile);
+
+			if (ResetFile == "1")
+			{
+				SaveState.open("SaveState.txt", fstream::out);
+
+				if (!SaveState)
+				{
+					system("CLS");
+					cout << "Error accessing file" << endl;
+					break;
+				}
+				else
+				{
+					SaveState << "0";
+					if (SaveState.fail())			break;
+				}
+
+				SaveState.close();
+
+				cout << "                                                         File Wiped!";
+			}
+			else if (ResetFile == "2")
+			{
+				cout << "                                                  Returning to menu screen";
+				Sleep(800);
+				cout << ".";
+				Sleep(800);
+				cout << ".";
+				Sleep(800);
+				cout << ".";
+				Sleep(800);
+				break;
+			}
+		}
+		else
+		{
+			cout << "                                             The savestate has been tampered with, please reset";
+			Sleep(1200);
+		}
+	}
+	system("CLS");
+}
+
+void ContinueGame()
+{
+	int GameState = 0, LoopForever = 0;
+	string StartNewGame;
+
+	while (LoopForever == 0)
+	{
+		cout << "     +    __________       +           *        ____  ___        *                +        +           *         ____" << endl;
+		cout << "         /__    ___/  ________   ________      /    |/   |     __________     _________   ___   ___ . ________ +/_  /" << endl;
+		cout << "       .   /   /     /  ___  /  /  _____/     /          |    /  ___    /    /  ___   /  /  /  /  /  /  _____/   /_/" << endl;
+		cout << "    .     /   / +   /  _____/  /  /____   *  /  /|   /|  |   /  /__/    |   /  /__/  /  /  /__/  /  /_____  /  ." << endl;
+		cout << "         /___/     /_______>  /_______/     /__/ |__/ |__|  /________/|_|  /_____   /  /________/  /_______/ ." << endl;
+		cout << "       *       .       .        *       +         .       *               ______/  /            .               ." << endl;
+		cout << "   .                                +                  .         .       /________/     +                 ." << endl;
+		cout << "      ______ .           +                     +              ____     .              .     *          ____     +" << endl;
+		cout << "     /  _   |     ________ . ________   ____ + ________   ___/   /__   __________ *   __ _______ + ___/   /__ ." << endl;
+		cout << "    /  /_|  |    /  _____/  /  _____/. /   /  /  _____/  /__    ___/  /  ___    /    / // __   /  /__    ___/" << endl;
+		cout << "   /  ___   |+  /_____  /  /_____  / ./   /  /_____  /  .  /   /     /  /__/    |   /    / /  /   . /   / ." << endl;
+		cout << "  /__/   |__|  /_______/ +/_______/  /___/  /_______/     /___/   + /________/|_|  /____/ /__/     /___/     *" << endl;
+		cout << " *  .     +          .         *      .          +            .      .          +        .    .        +" << endl;
+
+		fstream SaveState;
+		SaveState.open("GameCompletionState.txt", fstream::in);
+
+		if (!SaveState)
+		{
+			system("CLS");
+			cout << "Error accessing file" << endl;
+			break;
+		}
+		else
+			SaveState >> GameState;
+
+		SaveState.close();
+
+		if (GameState == 0)
+		{
+			cout << "\n                            A save file currently does not exist, do you want to make a new one?\n" << endl;
+			cout << "                                                         1 . Y e s" << endl;
+			cout << "                                                         2 . N o\n" << endl;
+			cout << "                                                Enter Number to select option: ";
+			getline(cin, StartNewGame);
+
+			if (StartNewGame == "1")
+			{
+				NewGame();
+				break;
+			}
+			else if (StartNewGame == "2")
+			{
+				cout << "                                                  Returning to menu screen";
+				Sleep(800);
+				cout << ".";
+				Sleep(800);
+				cout << ".";
+				Sleep(800);
+				cout << ".";
+				Sleep(800);
+			}
+			break;
+		}
+		else if (GameState == 1)
+		{
+			cout << "\n                                                        Starting Game";
+			Sleep(800);
+			cout << ".";
+			Sleep(800);
+			cout << ".";
+			Sleep(800);
+			cout << ".";
+			Sleep(800);
+
+			GameStart = 1;
+		}
+		else
+		{
+			cout << "                                             The savestate has been tampered with, please reset";
+			Sleep(1200);
+		}
+	}
+	system("CLS");
 }
 
 void LearnCpp()
 {
+	int LoopForever = 0;
+	while (LoopForever == 0)
+	{
+		ifstream ChapSave("ChapSaveState.txt");
+		if (!ChapSave)
+		{
+			system("CLS");
+			cout << "Error accessing file" << endl;
+			break;
+		}
+		else
+			cout << "nigger" << endl;
+	}
 
+	
+
+	
 }
 
 int Dialogue()
 {
 	// aaron you put the finalised text processing thing here please
+	return 0;
 }
 
 int Chap1()
 {
-
+	return 0;
 
 
 }
 
 int Chap2()
 {
-
+	return 0;
 
 }
 
 int Chap3()
 {
-
+	return 0;
 
 
 }
 
 int Chap4()
 {
-
+	return 0;
 }
 
 int Chap5()
 {
-
+	return 0;
 }
 
 int Chap6()
 {
-
+	return 0;
 }
 
 int Chap7()
 {
-
+	return 0;
 }
 
 int Chap8()
 {
-
+	return 0;
 }
 
 int Chap9()
 {
-
+	return 0;
 }
 
 int Chap10()
 {
-
+	return 0;
 }
 
 void GameOver()
 {
-
-
 
 }

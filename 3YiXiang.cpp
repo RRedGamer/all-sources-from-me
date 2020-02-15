df48669b4bc9a9d3fa81c2829a0e670b4738c5a2
RRedGamer <59791373+RRedGamer@users.noreply.github.com>
2020-02-15 11:28:00 +0800

    Update 3YiXiang.cpp
    New shit yeet

--- 3YiXiang.cpp
+++ 3YiXiang.cpp
@@ -1,227 +1,209 @@
-void Battle(char ChpNum)
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <iomanip>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+using namespace std::chrono;
+
+int maxBar[4], currentBar[4];
+string level[4];
+
+int TimerSystem(int Mode);
+void AFK();
+void displayBar(int maxBar, int currentBar);
+void AdjustStats(int AdjustedLevel, int AdjustedAmt);
+
+int main()
 {
-	int EnemyHP = 5, PlayerHP = 3, QuestionNum = 1;
-	string BattleTextfile, QnOutput, CorrectAnswer = "InvalidAns", InputAnswer;
-	bool ActualWord = TRUE;
+	int i = 0;
+	fstream emotionFile;
+	string response;
+	bool valid = false;
 
-	srand(time(NULL));
-
-	char FilenameBuilder[20] = { 'B','a','t','t','l','e','C','h','p', ChpNum , '.', 't', 'x', 't' };
-	BattleTextfile = FilenameBuilder;
-
-	fstream BattleQns;
-	BattleQns.open(BattleTextfile, fstream::in);
-
-	if (!BattleQns)
-		cout << "Error accessing file" << endl;
+	cout << left;
+	emotionFile.open("emotionStates.txt", fstream::in);
+	if (!emotionFile)	cout << "Error accessing file.\n";
 	else
 	{
-		while (!BattleQns.eof())
+		while (!emotionFile.eof())
 		{
-			while (EnemyHP != 0 && PlayerHP != 0)
-			{
-				cout << "Question " << QuestionNum << ":" << endl;
-				cout << "=========================================================================" << endl;
-
-				if (BattleQns.fail())			break;
-				while (QnOutput != "EndQn")
-				{
-					BattleQns >> QnOutput;
-
-					if (QnOutput == "n/" || QnOutput == "EndQn")
-						cout << endl;
-					else if (QnOutput == "t/")
-						cout << "   ";
-					else
-						cout << QnOutput << " ";
-					
-					Sleep(100);
-				}
-				cout << "=========================================================================" << endl;
-				QnOutput = ".";
-				QuestionNum++;
-
-				BattleQns >> CorrectAnswer;
-				cout << "Fill in the blank: ";
-				cin >> InputAnswer;
-
-				system("cls");
-				cout << "Enemy HP: " << EnemyHP << endl;
-				cout << "Your HP: " << PlayerHP << endl;
-
-				if (InputAnswer == CorrectAnswer)
-				{
-					EnemyHP--;
-				}
-				else
-				{
-					PlayerHP--;
-				}
-
-				Sleep(1000);
-
-				system("cls");
-				cout << "Enemy HP: " << EnemyHP << endl;
-				cout << "Your HP: " << PlayerHP << endl;
-
-				Sleep(1000);
-				system("cls");
-
-				CorrectAnswer = "InvalidAns";
-			}
+			emotionFile >> level[i] >> maxBar[i] >> currentBar[i];
+			if (emotionFile.fail())	break;
+			cout << setw(15) << level[i];
+			displayBar(maxBar[i], currentBar[i]);
+			i++;
 		}
+		emotionFile.close();
 	}
+	AFK();
+	cout << "Do you want to play with the pet? (Y/N)\t";
+	cin >> response;
 
-	system("pause");
-}
-
-
-void BattleMechanics(char ChpNum, int EnemyHP, int PlayerHP)
-{
-	int QuestionNum = 1;
-	string CorrectAnswer = "Invalid_Ans", InputAnswer;
-
-	while (EnemyHP != 0 && PlayerHP != 0)
+	while (valid == false)
 	{
-		cout << "Question " << QuestionNum << ":" << endl;
-
-		CorrectAnswer = BattleTextProcessor(ChpNum, QuestionNum);
-
-		QuestionNum++;
-
-		cin >> InputAnswer;
-
-		system("cls");
-		cout << "Enemy HP: " << EnemyHP << endl;
-		cout << "Your HP: " << PlayerHP << endl;
-
-		if (InputAnswer == CorrectAnswer)
+		if (response == "Y")
 		{
-			EnemyHP--;
+			AdjustStats(0, 1);
+			AdjustStats(3, 1);
+			valid = true;
+		}
+		else if (response == "N")
+		{
+			AdjustStats(0, -1);
+			AdjustStats(3, -1);
+			valid = true;
 		}
 		else
 		{
-			PlayerHP--;
+			cout << "Invalid response given\n";
+			cout << "Do you want to play with the pet?\t";
+			cin >> response;
 		}
-
-		Sleep(1000);
-
-		system("cls");
-		cout << "Enemy HP: " << EnemyHP << endl;
-		cout << "Your HP: " << PlayerHP << endl;
-
-		Sleep(1000);
-		system("cls");
-
-		CorrectAnswer = "InvalidAns";
 	}
 
-	if (EnemyHP == 0)
-		return;
-	else if (PlayerHP == 0)
-		GameOver();
+	for (int i = 0; i <= 3; i++)
+	{
+		cout << setw(15) << level[i];
+		displayBar(maxBar[i], currentBar[i]);
+	}
+	emotionFile.open("emotionStates.txt", fstream::out);
+	for (int i = 0; i <= 3; i++)
+	{
+		emotionFile << level[i] << " " << maxBar[i] << " " << currentBar[i] << endl;
+	}
+	emotionFile.close();
 
 	system("pause");
+	return 0;
 }
 
-string BattleTextProcessor(char ChpNum, int QuestionNum)
+int TimerSystem(int Mode)
 {
-	int CurrentQn = 1;
-	string BattleTextFile, QnsOutput = ".", Answer;
+	int CurrentTimeStamp, PreviousTimeStamp, TimeElapsed, TurnsElapsed, CurrentTurn;
+	time_t TimeStamp; //datatype we need for storing time
+	fstream TimeCheck;
+	TimeCheck.open("TimeStamp.txt", fstream::in);
 
-	char FileNameBuilder[20] = { 'B','a','t','t','l','e','C','h','p', ChpNum , '.', 't', 'x', 't' };
-
-	BattleTextFile = FileNameBuilder;
-
-	ifstream BattleQns;
-	BattleQns.open(BattleTextFile);
-
-	if (!BattleQns)
-	{
-		system("cls");
-		cout << "Error accessing file" << endl;
-		Sleep(3000);
-	}
+	if (!TimeCheck)
+		cout << "Failed to retrive file" << endl;
 	else
 	{
-		while (!BattleQns.eof())
-		{
-			BattleQns >> QnsOutput;
-			while (CurrentQn < QuestionNum)
-			{
-				if (BattleQns.fail())			break;
-				if (QnsOutput == "[Answer]")
-					CurrentQn++;
-
-				BattleQns >> QnsOutput;
-			}
-			if (BattleQns.fail())			break;
-
-			while (QnsOutput != "[EndQn]")
-			{
-				if (QnsOutput == "n/")
-					cout << endl;
-				else if (QnsOutput == "t/")
-					cout << "    ";
-				else
-					cout << QnsOutput << " ";
-				BattleQns >> QnsOutput;
-
-				Sleep(100);
-			}
-
-			BattleQns >> Answer >> QnsOutput;
-
-			if (QnsOutput != "[Answer]")
-			{
-				system("cls");
-				cout << "The file has been tempered with, please contact the developers" << endl;
-			}
-			else
-				return Answer;
-		}
+		TimeCheck >> PreviousTimeStamp >> CurrentTurn;
 	}
+	TimeCheck.close();
+
+	system_clock::time_point tp = system_clock::now(); //obtains current timepoint for reference
+	system_clock::duration dtn = tp.time_since_epoch(); //obtains the lapsed time since epoch (Jan 01 1970 00:00)
+
+	TimeStamp = dtn.count() * system_clock::period::num / system_clock::period::den; //converts the lapsed time in periods to seconds, and stores it in TimeStamp
+	CurrentTimeStamp = TimeStamp;
+	
+	TimeElapsed = CurrentTimeStamp - PreviousTimeStamp;
+	TurnsElapsed = TimeElapsed / 3600;
+	
+	if (Mode == 1)
+		CurrentTurn += TurnsElapsed;
+	if (Mode == 2)
+		return CurrentTurn;
+	else if (Mode == 3)
+		CurrentTurn++;
+		
+	TimeCheck.open("TimeStamp.txt", fstream::out);
+
+	if (!TimeCheck)
+		cout << "error overwritting previous timestamp!" << endl;
+	else
+		TimeCheck << CurrentTimeStamp << " " << CurrentTurn;
+
+	TimeCheck.close();
+
+	if (Mode == 1)
+		return TurnsElapsed;
+	else if (Mode == 3)
+		return CurrentTurn;
 }
 
-void GameOver()
+void AFK()
 {
-	Sleep(500);
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << "                                                    ";
-	cout << "G";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "A";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "M";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "E";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "O";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "V";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "E";	Sleep(150);
-	cout << " ";	Sleep(150);
-	cout << "R";
+	int StatChoice, TurnsElapsed, HappinessDecrease = 0, FoodDecrease = 0, WaterDecrease = 0, CleanlinessDecrease = 0;
+	srand(time(NULL));
 
-	Sleep(2000);
+	TurnsElapsed = TimerSystem(1);
+
+	if (TurnsElapsed == 0)
+		return;
+
+	for (int i = 0; i < TurnsElapsed; i++)
+	{
+		StatChoice = rand() % 4;
+		AdjustStats(StatChoice, -1);
+
+		if (StatChoice == 0)
+			HappinessDecrease++;
+		else if (StatChoice == 1)
+			FoodDecrease++;
+		else if (StatChoice == 2)
+			WaterDecrease++;
+		else if (StatChoice == 3)
+			CleanlinessDecrease++;
+	}
+
+	cout << "While you were away, " << TurnsElapsed << " turn(s) have passed and your pet's ";
+	if (HappinessDecrease != 0)
+		cout << "happiness dropped by " << HappinessDecrease;
+	if (FoodDecrease != 0)
+	{
+		if (HappinessDecrease != 0)
+		{
+			if (WaterDecrease == 0 && CleanlinessDecrease == 0)
+				cout << " & ";
+			else
+				cout << ", ";
+		}
+		cout << "food dropped by " << FoodDecrease;
+	}
+	if (WaterDecrease != 0)
+	{
+		if (HappinessDecrease != 0 || FoodDecrease != 0)
+		{
+			if (CleanlinessDecrease == 0)
+				cout << " & ";
+			else 
+				cout << ", ";
+		}
+		cout << "water dropped by " << WaterDecrease;
+	}
+	if (CleanlinessDecrease != 0)
+	{
+		if (HappinessDecrease != 0 || FoodDecrease != 0 || WaterDecrease != 0)
+			cout << " & ";
+		cout << "cleanliness dropped by " << CleanlinessDecrease;
+	}
+
 	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << "                                               ";
-	system("pause");
-	Sleep(1000);
-	system("color 08");
-	Sleep(1000);
-	system("cls");
-	Sleep(4000);
+	return;
+}
+
+void displayBar(int maxBar, int currentBar)
+{
+	cout << "[";
+	for (int i = 0; i < currentBar; i++)			cout << "=";
+	for (int j = 0; j < maxBar - currentBar; j++)	cout << " ";
+	cout << "]\n";
+}
+
+void AdjustStats(int AdjustedLevel, int AdjustedAmt)
+{
+	if (AdjustedAmt <= maxBar[AdjustedLevel])
+		currentBar[AdjustedLevel] += AdjustedAmt;
+
+	if (currentBar[AdjustedLevel] > 10)
+		currentBar[AdjustedLevel] = 10;
+	else if (currentBar[AdjustedLevel] < 0)
+		currentBar[AdjustedLevel] = 0;
 }
